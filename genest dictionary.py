ca45671431399eb1652c57e850f6ba2feef7f59c
bjornlecis <82440809+bjornlecis@users.noreply.github.com>
2021-12-20 21:21:36 +0100

    Create genest dictionary.py

---
+++ genest dictionary.py
@@ -0,0 +1,18 @@
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+
+kind1 = (myfamily["child3"].values())
+for x in kind1:
+    print(x)
